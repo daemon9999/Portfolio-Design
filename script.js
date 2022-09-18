41a4d273097b1406ac590a61c5c560bffa59ef95
daemon9999 <murad.mesimli.99@gmail.com>
2022-09-18 22:46:31 +0400

    Design coded from Figma

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function exit(){
+    document.getElementById("portfolio").style.display = "none";
+    document.getElementById("enter").style.display = "flex";
+}
+
+function enter(){
+    document.getElementById("portfolio").style.display = "flex";
+    document.getElementById("enter").style.display = "none";
+}
